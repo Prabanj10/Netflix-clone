78af88e5a95fb8895fb33a404a1564591cbe72ff
Prabanj <prabanjk@gmail.com>
2024-10-06 03:04:04 +0530

    Added search bar with functionality to display matching movie names and posters.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react';
 import './NavBar.css';
+import axios from '../../axios';
+import { imageUrl } from '../../constants/constants';
+
+const apikey = import.meta.env.VITE_API_KEY;
 
 const NavBar = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    input &&
+      axios
+        .get(`/search/movie?query=${input}&api_key=${apikey}`)
+        .then((res) => {
+          console.log(res.data.results);
+          setResult(res.data.results);
+        });
+  }, [input]);
+
+  const handleSearch = (e) => {
+    setInput(e.target.value);
+    console.log(input);
+  };
+
   return (
     <div className="navbar">
       <img
@@ -13,6 +36,36 @@ const NavBar = () => {
         src="https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-vnl1thqrh02x7ra2.webp"
         alt=""
       />
+      <div className="searchBox">
+        <input
+          placeholder="Search"
+          type="text"
+          className="searchBar"
+          value={input}
+          onChange={handleSearch}
+        />
+        {input && (
+          <div className="searchResult">
+            {result.length ? (
+              result.slice(0, 5).map((item) => (
+                <div
+                  className="resultBox"
+                  key={item.id}>
+                  <img
+                    className="resultImage"
+                    src={imageUrl + item.poster_path}
+                    alt=""
+                  />
+
+                  <p className="resultTitle">{item.title}</p>
+                </div>
+              ))
+            ) : (
+              <p>No result</p>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
